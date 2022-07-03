210177f333d4e63ef57baa514e9f67ea99779aad
Juliana Amancio <jhullyjoviano@gmail.com>
2022-07-03 19:47:43 -0300

    setup inicial

---
+++ src/Routes.tsx
@@ -0,0 +1,19 @@
+import { BrowserRouter, Route, Routes} from "react-router-dom";
+import Home from "./pages/Home";
+import Profile from "./pages/Profile";
+import Registration from "./pages/Registration";
+
+  
+const MyRoutes: React.FC = () => {
+ return (
+    <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/Registration" element={<Registration />} />
+        </Routes>
+    </BrowserRouter>
+    );
+  };
+  
+  export default MyRoutes;
---
+++ src/pages/Profile/index.tsx
@@ -0,0 +1,7 @@
+function Profile (){
+    return(
+        <h1>Hello Word</h1>
+
+    )
+}
+export default Profile
